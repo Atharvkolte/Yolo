734307f932f3f715710077f3d9105cb0bac20c97
Atharva <atharv31kolte@gmail.com>
2025-07-23 20:46:16 +0530

    Stage 3 Done

--- 3)LearningYolo.py
+++ 3)LearningYolo.py
@@ -1,8 +1,8 @@
 #Make camera working ✔️
 #detect person ✔️
-#and unusal object like knife or bottle 
-#when unsual object is detect make sound and take photo/video
-#Display the warning sign
+#and unusal object like knife or bottle ✔️
+#when unsual object is detect make sound and take photo/video✔️
+#Display the warning sign✔️
 
 ###
 #Add timestamp when detected
@@ -13,6 +13,18 @@
 import cv2
 import time
 from ultralytics import YOLO
+import winsound
+import time
+from datetime import datetime
+
+def alertSound():
+    winsound.Beep(1000, 500)  # 1000 Hz for 500 ms
+    time.sleep(0.2)           # Short pause
+
+def alertTriggered(img):
+    timestamp=datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+    filename=f"CheckingDetection/detected_{timestamp}.jpg"
+    cv2.imwrite(filename,img)
 
 cap=cv2.VideoCapture(0)
 model=YOLO("yolov8n.pt")
@@ -23,6 +35,24 @@ while True:
     results=model.predict(source=frame,stream=False,verbose=False,iou=0.5,conf=0.5)
     annotated = results[0].plot()
     
+    alert_triggered=False
+    for result in results:
+        boxes=result.boxes
+        for box in boxes:
+            class_id=int(box.cls)
+            class_name=model.names[class_id]
+            conf=float(box.conf)
+            
+            if class_name in ALERT_CLASSES:
+                alertSound()
+                alert_triggered=True
+                print(f"Alert:{class_name} Detected")
+                cv2.putText(annotated,f"detected:{class_name}",(20,70),cv2.FONT_HERSHEY_COMPLEX,1,(225,0,225),2)
+                
+    if alert_triggered:
+        alertTriggered(annotated)
+        
+
     cv2.imshow("Webcam",annotated)
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
